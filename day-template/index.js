ce27d4f7141f5472f7a3733865e1dbb2c5942f74
Anthony Suarez-McGrath <akm@anthonykyle.co.nz>
2025-05-27 11:08:03 +1200

    Update day Template to use file imports and improved tests

--- day-template/index.js
+++ day-template/index.js
@@ -1,7 +1,21 @@
+import fs from 'fs';
+
 export class DayClass {
+  filePath;
+
   instructions;
 
-  constructor(instructions) {
-    this.instructions = instructions
+  readFile() {
+    const fileContents = fs.readFileSync(this.filePath, 'utf8');
+    return fileContents.trim().split('\n');
+  }
+
+  parseInstructions() {
+    this.instructions = this.readFile();
+  }
+
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.parseInstructions();
   }
 }
--- day-template/test/test.mjs
+++ day-template/test/test.mjs
@@ -1,39 +1,50 @@
 import assert from 'assert';
 import chalk from 'chalk';
 import { DayClass } from '../index.js'; // Ensure the main module is loaded
-import directions from '../input.js'; // Import the input file
 
-describe('Part 1: ', function() {
+describe('Part 1:', function() {
   const cases = [
   ];
 
   cases.forEach(function(testCase) {
-    it(`should return ${testCase[1]} for input "${testCase[0]}"`, function() {
-      const dayInstance = new DayClass(testCase[0]);
+    it(`Should return ${testCase[1]} for input "${testCase[0]}"`, function() {
+      const filePath = `./inputs/${testCase[0]}`;
+      const dayInstance = new DayClass(filePath);
       assert.strictEqual(dayInstance?.targetProp, testCase[1]);
     });
   })
 
-  console.log(chalk.blue('======================================================='));
-  const part1FinalInstance = new DayClass(directions);
-  console.log('Part 1 Answer:', chalk.green(part1FinalInstance?.targetProp));
-  console.log(chalk.blue('======================================================='));
 });
 
-describe('Part 2: ', function() {
+describe('Part 2:', function() {
   const cases = [
-
   ];
 
   cases.forEach(function(testCase) {
-    it(`should return ${testCase[1]} for input "${testCase[0]}"`, function() {
-      const dayInstance = new DayClass(testCase[0]);
+    it(`Should return ${testCase[1]} for input "${testCase[0]}"`, function() {
+      const filePath = `./inputs/${testCase[0]}`;
+      const dayInstance = new DayClass(filePath);
       assert.strictEqual(dayInstance?.targetProp, testCase[1]);
     });
   })
+});
 
+describe('Running Final Tests:', () => {
+  const finalInstance = new DayClass('./inputs/testInput');
+  const part1Result = finalInstance?.targetProp;
+  const part2Result = finalInstance?.targetProp;
   console.log(chalk.blue('======================================================='));
-  const part2FinalInstance = new DayClass(directions);
-  console.log('Part 2 Answer:', chalk.green(part2FinalInstance?.targetProp))
+  if (part1Result) {
+    console.log('Part 1 Answer:', chalk.green(part1Result));
+    console.log(chalk.blue('-------------------------------------------------------'));
+  }
+  if (part2Result) {
+    console.log('Part 2 Answer:', chalk.green(part2Result));
+  }
   console.log(chalk.blue('======================================================='));
-})
+
+  it ('Should have valid results for both parts', function() {
+    assert.ok(part1Result, 'Part 1 result should not be undefined');
+    assert.ok(part2Result, 'Part 2 result should not be undefined');
+  });
+});
